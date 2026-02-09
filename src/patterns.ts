0d4e0b5d36bf4a5dce4f679bf63f100a75adc58d
Naresh Bhosale <91016022+WebNaresh@users.noreply.github.com>
2026-02-09 15:52:59 +0530

    refactor: simplify URL matching regex patterns and update package version.

--- src/patterns.ts
+++ src/patterns.ts
@@ -21,7 +21,7 @@ export const API_PATTERNS: RegExp[] = [
   /['"`]\/api\/([^'"`\s]+)['"`]/g,
 
   // Full URL matching (http://.../api/...)
-  /['"`](?:https?:\/\/[^\/]+)?\/api\/([^'"`\s]+)['"`]/g,
+  /['"`](?:https?:\/\/[^/]+)?\/api\/([^'"`\s]+)['"`]/g,
 ];
 
 /**
@@ -50,7 +50,7 @@ export const API_METHOD_PATTERNS: { regex: RegExp; method?: string }[] = [
   { regex: /fetch\s*\(\s*['"`]\/api\/([^'"`\s)]+)['"`]/g, method: undefined },
   { regex: /fetch\s*\(\s*`\/api\/([^`\s)]+)`/g, method: undefined },
   { regex: /['"`]\/api\/([^'"`\s]+)['"`]/g, method: undefined },
-  { regex: /['"`](?:https?:\/\/[^\/]+)?\/api\/([^'"`\s]+)['"`]/g, method: undefined },
+  { regex: /['"`](?:https?:\/\/[^/]+)?\/api\/([^'"`\s]+)['"`]/g, method: undefined },
 ];
 
 /**
